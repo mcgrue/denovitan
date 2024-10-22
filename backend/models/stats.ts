4dfb2e92c3121401f75d8ab57fd31264238a1e27
Sebastián Rebaza <sebastianrebaz@gmail.com>
2024-10-22 13:49:03 -0500

    Completed Stat Declaration

--- backend/models/stats.ts
+++ backend/models/stats.ts
@@ -127,7 +127,6 @@ damage.set(
         "dmg_putrefying",
     ),
 );
-
 damage.set(
     "righteous",
     new Statistic(
@@ -273,7 +272,6 @@ resist.set(
         "dmg_putrefying_resist",
     ),
 );
-
 resist.set(
     "righteous",
     new Statistic(
@@ -390,5 +388,274 @@ primary.set(
         (war: number, rog: number, wiz: number) => {
             return war + rog * 2 + wiz * 2;
         },
+    ),
+);
+
+secondary.set(
+    0,
+    new Statistic(
+        "Life",
+        "Secondary",
+        "This number shows how not-dead you are. When it hits zero you cease to live. Try not to let that happen.",
+        "stat_life",
+        (war: number, rog: number, wiz: number) => {
+            return 5 + war * 4 + rog * 3 + wiz * 2;
+        },
+    ),
+);
+secondary.set(
+    1,
+    new Statistic(
+        "Mana",
+        "Secondary",
+        "This number shows your capacity to cast magic spells, all of which cost mana to some degree or other.",
+        "stat_mana",
+        (war: number, rog: number, wiz: number) => {
+            return war * 2 + rog * 2 + wiz * 4;
+        },
+    ),
+);
+secondary.set(
+    2,
+    new Statistic(
+        "Melee Power",
+        "Secondary",
+        "Melee Power affects how hard you hit with your weapons (or feet, if you don't have any). It's also how hard you throw stuff, but not how hard you shoot your crossbow bolts because that wouldn't make sense.",
+        "stat_meleepower",
+        (war: number, rog: number, wiz: number) => {
+            return Math.floor((war * 2 + rog + wiz - 5) / 3);
+        },
+    ),
+);
+secondary.set(
+    3,
+    new Statistic(
+        "Magic Power",
+        "Secondary",
+        "Magic Power affects the power of your spells.",
+        "stat_magicpower",
+        (war: number, rog: number, wiz: number) => {
+            return Math.floor((war + rog + wiz * 2) / 2);
+        },
+    ),
+);
+secondary.set(
+    4,
+    new Statistic(
+        "Crit",
+        "Secondary",
+        "Critical Chance affects how likely you are to land a particularly effective blow in combat, dealing double damage.",
+        "stat_crit",
+        (war: number, rog: number, wiz: number) => {
+            return Math.floor((war * 2 + rog * 2 + wiz) / 2);
+        },
+    ),
+);
+secondary.set(
+    5,
+    new Statistic(
+        "Haywire",
+        "Secondary",
+        "Haywire Chance affects how likely your spells are to trigger a highly unstable resonance cascade event.",
+        "stat_haywire",
+        (war: number, rog: number, wiz: number) => {
+            return Math.floor((war + rog * 2 + wiz * 2) / 2);
+        },
+    ),
+);
+secondary.set(
+    6,
+    new Statistic(
+        "Dodge",
+        "Secondary",
+        "Dodge Chance affects how likely you are to avoid the consequences of your actions and physical attacks.",
+        "stat_dodge",
+        (war: number, rog: number, wiz: number) => {
+            return Math.floor((war + rog * 2 + wiz) / 2);
+        },
+    ),
+);
+secondary.set(
+    7,
+    new Statistic(
+        "Block",
+        "Secondary",
+        "Block Chance affects how likely you are to (mostly) block what you deserve, at least so far as physical attacks go.",
+        "stat_block",
+        (war: number, rog: number, wiz: number) => {
+            return Math.floor(
+                (war * 2 + rog + wiz) / 6 + (war * 2 + rog + wiz * 2) / 6,
+            );
+        },
+    ),
+);
+secondary.set(
+    8,
+    new Statistic(
+        "Counter",
+        "Secondary",
+        "Counter Chance affects how likely you are to commit a vindictive act in melee combat by returning an enemy's attack with one of your own.",
+        "stat_counter",
+        (war: number, rog: number, wiz: number) => {
+            return Math.floor(
+                (war + rog * 2 + wiz) / 6 + (war * 2 + rog * 2 + wiz) / 6,
+            );
+        },
+    ),
+);
+secondary.set(
+    9,
+    new Statistic(
+        "Enemy Dodge Reduction",
+        "Secondary",
+        "Enemy Dodge Reduction affects how likely an enemy is to not dodge your attacks. Because heroes never miss - but sometimes their attacks are dodged.",
+        "stat_edr",
+        (war: number, rog: number, wiz: number) => {
+            return Math.floor((war + rog * 2 + wiz) / 3);
+        },
+    ),
+);
+secondary.set(
+    10,
+    new Statistic(
+        "Armour Absorption",
+        "Secondary",
+        "Armour Absorption is how good your armor is at standing between you and inevitability. Each point of Armour Absorption removes one point of mundane damage types.",
+        "stat_armourabsorption",
+    ),
+);
+secondary.set(
+    11,
+    new Statistic(
+        "Magic Resistance",
+        "Secondary",
+        "Affects how intuitively you can (mostly) nullify a magical attack against you.",
+        "stat_magicresistance",
+        (war: number, rog: number, wiz: number) => {
+            return Math.floor((war * 2 + rog + wiz * 2) / 2);
+        },
+    ),
+);
+secondary.set(
+    12,
+    new Statistic(
+        "Sneakiness",
+        "Secondary",
+        "Sneakiness is how good you are at tip-toeing around your problems rather than facing them. Helps with sneaking by enemies, not waking sleeping enemies, and with avoiding traps.",
+        "stat_sneakiness",
+        (war: number, rog: number, wiz: number) => {
+            return Math.max(
+                Math.floor((war * 2 + rog * 4 + wiz * 3) * 0.75) - 20,
+                0,
+            );
+        },
+    ),
+);
+secondary.set(
+    13,
+    new Statistic(
+        "Life Regen",
+        "Secondary",
+        "Listen up, this one is tricky: This number is your health regeneration bonus. It is subtracted from the base health regeneration rate to determine the number of steps it takes for you to regenerate one point of health. A higher number is better.",
+        "stat_liferegen",
+    ),
+);
+secondary.set(
+    14,
+    new Statistic(
+        "Mana Regen",
+        "Secondary",
+        "Listen up, this one is tricky: This number is your mana regeneration bonus. It is subtracted from the base mana regeneration rate to determine the number of steps it takes for you to regenerate one point of mana. A higher number is better.",
+        "stat_manaregen",
+    ),
+);
+secondary.set(
+    15,
+    new Statistic(
+        "Wand Affinity",
+        "Secondary",
+        "[[Deprecated stat.]]",
+        "stat_wandburn",
+    ),
+);
+secondary.set(
+    16,
+    new Statistic(
+        "Trap Affinity",
+        "Secondary",
+        "Trap Affinity is a rating of your ability to avoid and disarm traps.",
+        "stat_traplevel",
+    ),
+);
+secondary.set(
+    17,
+    new Statistic(
+        "Trap Sight Radius",
+        "Secondary",
+        "Trap Sight Radius determines the distance at which you can spot and identify traps.",
+        "stat_trapsense",
+    ),
+);
+secondary.set(
+    18,
+    new Statistic(
+        "Sight Radius",
+        "Secondary",
+        "Visual Sight Radius is how far you can see in the dark dungeon.",
+        "stat_sight",
+    ),
+);
+secondary.set(
+    19,
+    new Statistic(
+        "Smithing",
+        "Secondary",
+        "This is a rating of how good you are at smithery.",
+        "stat_smithing",
+    ),
+);
+secondary.set(
+    20,
+    new Statistic(
+        "Tinkering",
+        "Secondary",
+        "This is a rating of how good you are at tinkering.",
+        "stat_tinkerer",
+    ),
+);
+secondary.set(
+    21,
+    new Statistic(
+        "Alchemy",
+        "Secondary",
+        "This is a rating of how good you are at alchemizing.",
+        "stat_alchemy",
+    ),
+);
+secondary.set(
+    22,
+    new Statistic(
+        "Magic Reflection",
+        "Secondary",
+        "[[No Description -- chance to reflect projectiles]]",
+        "stat_reflection",
+    ),
+);
+secondary.set(
+    23,
+    new Statistic(
+        "Wand Crafting",
+        "Secondary",
+        "This is a rating of how good you are at wandcrafting.",
+        "stat_wandburn",
+    ),
+);
+secondary.set(
+    99,
+    new Statistic(
+        "Experience Points",
+        "Secondary",
+        "[[No Description -- used to discern how close the player is to a levelup.",
+        "xp_medal",
     ),
 );
