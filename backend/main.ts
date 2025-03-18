0a7fca131a847bc8ad297a31bb73d94def3c4606
Ben McGraw <ben.mcgraw@gmail.com>
2025-03-17 20:45:27 -0700

    Stop serving the static files from the server automatically

--- backend/main.ts
+++ backend/main.ts
@@ -1,13 +1,12 @@
 import { Application, Router } from "jsr:@oak/oak";
 import { oakCors } from "jsr:@tajpouria/cors";
 import {
-	createFileWithContent,
-	deleteFileIfExists,
+  createFileWithContent,
+  deleteFileIfExists,
 } from "../deno-common/file.ts";
 import { getFirstAvailablePort } from "../deno-common/net.ts";
 import { BACKEND_PORT_FILE } from "./constants.ts";
 import data from "./data.json" with { type: "json" };
-import routeStaticFilesFrom from "./util/routeStaticFilesFrom.ts";
 
 await deleteFileIfExists(BACKEND_PORT_FILE);
 
@@ -17,50 +16,51 @@ const port = await getFirstAvailablePort(startingBackendPort);
 console.log(`[${Deno.pid}] found available backend port: ${port}`);
 
 try {
-	await createFileWithContent(BACKEND_PORT_FILE, port.toString());
-	// deno-lint-ignore no-explicit-any
+  await createFileWithContent(BACKEND_PORT_FILE, port.toString());
+  // deno-lint-ignore no-explicit-any
 } catch (e: any) {
-	console.error(`[${Deno.pid}] ${e.message}`);
-	Deno.exit(1);
+  console.error(`[${Deno.pid}] ${e.message}`);
+  Deno.exit(1);
 }
 
 try {
-	const router = new Router();
+  const router = new Router();
 
-	// deno-lint-ignore no-explicit-any
-	router.get("/api/dinosaurs", (context: any) => {
-		context.response.body = data;
-	});
+  // deno-lint-ignore no-explicit-any
+  router.get("/api/dinosaurs", (context: any) => {
+    context.response.body = data;
+  });
 
-	// deno-lint-ignore no-explicit-any
-	router.get("/api/dinosaurs/:dinosaur", (context: any) => {
-		if (!context?.params?.dinosaur) {
-			context.response.body = "No dinosaur name provided.";
-		}
+  // deno-lint-ignore no-explicit-any
+  router.get("/api/dinosaurs/:dinosaur", (context: any) => {
+    if (!context?.params?.dinosaur) {
+      context.response.body = "No dinosaur name provided.";
+    }
 
-		const dinosaur = data.find((item) =>
-			item.name.toLowerCase() === context.params.dinosaur.toLowerCase()
-		);
+    const dinosaur = data.find((item) =>
+      item.name.toLowerCase() === context.params.dinosaur.toLowerCase()
+    );
 
-		context.response.body = dinosaur ? dinosaur : "No dinosaur found.";
-	});
+    context.response.body = dinosaur ? dinosaur : "No dinosaur found.";
+  });
 
-	const app = new Application();
-	app.use(oakCors());
-	app.use(router.routes());
-	app.use(router.allowedMethods());
-	app.use(routeStaticFilesFrom([
-		`${Deno.cwd()}/dist`,
-		`${Deno.cwd()}/public`,
-	]));
+  const app = new Application();
+  app.use(oakCors());
+  app.use(router.routes());
+  app.use(router.allowedMethods());
 
-	console.log(
-		`[${Deno.pid}] Starting Deno-Vite Server running on http://localhost:${port}`,
-	);
-	await app.listen({ port: port });
+  // add an error page to route '/'
+  router.get("", (context: any) => {
+    context.response.body = "Backend services are running.";
+  });
 
-	// deno-lint-ignore no-explicit-any
+  console.log(
+    `[${Deno.pid}] Starting Deno-Vite Server running on http://localhost:${port}`,
+  );
+  await app.listen({ port: port });
+
+  // deno-lint-ignore no-explicit-any
 } catch (e: any) {
-	console.error(`[${Deno.pid}] ${e.message}`);
-	Deno.exit(1);
+  console.error(`[${Deno.pid}] ${e.message}`);
+  Deno.exit(1);
 }
